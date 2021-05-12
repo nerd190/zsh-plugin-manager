4d6d06dbddef6f6cec6266f8a3fb325c042975ec
Troels Bjørnskov <troels@bjoernskov.org>
2021-05-12 22:03:17 +0200

    Use $@ as args and fix whitespace

--- zsh-plugin-manager.zsh
+++ zsh-plugin-manager.zsh
@@ -4,10 +4,7 @@ declare -aU __asynchronous_plugins
 export PLUGROOT="${ZDOTDIR}/plugins"
 
 plug() {
-    printf "\x1b[?25l"            # hide the cursor while we update
-    local args=($@)
-    set --
-    case "${args[1]}" in
+    case "${@[1]}" in
         (init)
         __synchronous_plugins+=${__asynchronous_plugins:+romkatv/zsh-defer}
         __plug_init __synchronous_plugins
@@ -26,17 +23,17 @@ plug() {
         unset force
         ;;
         (async)
-        __asynchronous_plugins+="${args:6}"
+        shift
+        __asynchronous_plugins+="$@"
         ;;
         (*)
-        if [[ "${args}" != *"/"* ]]; then
-            printf "\r\x1B[3m${args}\033[0m does not look like a plugin and is not an action\033[0m\n"
+        if [[ "${@[1]}" != *"/"* ]]; then
+            printf "\r\x1B[3m$@\033[0m does not look like a plugin and is not an action\033[0m\n"
             return 1
         fi
-        __synchronous_plugins+="${args}"
+        __synchronous_plugins+="$@"
         ;;
     esac
-    printf "\x1b[?25h"            # show the cursor again
 }
 
 compile_or_recompile() {
@@ -44,16 +41,59 @@ compile_or_recompile() {
         || [[ "${1}" -nt "${1}.zwc" ]]; then
             zcompile "$1"
         fi
-    }
+}
 
-    __plug_update() {
-        local pluglist=($@)
-        set --
-        local plugin
-        for plugin in "${pluglist[@]}"; do
-        unset where plugindir github_name
+__plug_update() {
+    local pluglist=($@)
+    set --
+    local plugin
+    for plugin in "${pluglist[@]}"; do
+    unset where plugindir github_name
+    parts=("${(@s[, ])plugin}")
+    local github_name="${parts[1]}"
+    for part in "${parts[@]:1}"; do
+        key="${part%%:*}"
+        value="${part#*:}"
+        case "${key}" in
+            (if)
+            eval "${value}" > /dev/null 2>&1 || continue 2
+            ;;
+            (where)
+            local where="${(e)value}"
+            ;;
+            (*)
+            ;;
+            esac
+        done
+
+        plugindir="${where:-${PLUGROOT}/$github_name}"
+
+        printf "Updating \x1B[35m\033[3m${(r:40:: :)github_name} \033[0m … "
+        if git -C ${plugindir} pull 2> /dev/null; then
+            continue
+        elif [[ -n $force ]]; then
+            git -C ${plugindir} reset --hard HEAD
+            git -C ${plugindir} pull 2> /dev/null
+        else
+            printf "\x1B[31mFailed to update\033[0m\n"
+            continue 1
+        fi
+    done
+    printf "\x1B[32mIf plugins were updated, you should restart your shell\033[0m\n"
+}
+
+__plug_init() {
+    printf "\x1b[?25l"            # hide the cursor while we update
+    [[ $1 == __asynchronous_plugins ]] && source_cmd=("zsh-defer")
+    source_cmd+="source"
+    local input=${1}
+    set --
+    for plugin in ${${(P)input}}; do
+        unset nosource github_name filename plugindir preload postload postinstall where fetchcommand
+        # split strings by args
         parts=("${(@s[, ])plugin}")
-        local github_name="${parts[1]}"
+        github_name="${parts[1]}"
+
         for part in "${parts[@]:1}"; do
             key="${part%%:*}"
             value="${part#*:}"
@@ -61,140 +101,99 @@ compile_or_recompile() {
                 (if)
                 eval "${value}" > /dev/null 2>&1 || continue 2
                 ;;
+                (nosource)
+                if [[ "${${value}:l}" == "true" ]] || [[ "${value}" -eq 1 ]]; then
+                    nosource=true
+                fi
+                ;;
+                (postinstall)
+                postinstall="${postinstall:+$postinstall; }${value}"
+                ;;
+                (preload)
+                preload="${preload:+$preload; }${value}"
+                ;;
+                (postload)
+                postload="${postload:+$postload; }${value}"
+                ;;
                 (where)
-                local where="${(e)value}"
+                where="${(e)value}"
                 ;;
                 (*)
+                printf "\r\x1B[31mDid not understand the key: \033[0m\x1B[3m"${part}"\033[0m\nSkipping \x1B[35m"${github_name}"\033[0m plugin\n"
+                continue 2
                 ;;
-                esac
-            done
-
-            plugindir="${where:-${PLUGROOT}/$github_name}"
-
-            printf "Updating \x1B[35m\033[3m${(r:40:: :)github_name} \033[0m … "
-            if git -C ${plugindir} pull 2> /dev/null; then
-                continue
-            elif [[ -n $force ]]; then
-                git -C ${plugindir} reset --hard HEAD
-                git -C ${plugindir} pull 2> /dev/null
-            else
-                printf "\x1B[31mFailed to update\033[0m\n"
-                continue 1
-            fi
+            esac
         done
-        printf "\x1B[32mIf plugins were updated, you should restart your shell\033[0m\n"
-    }
 
-    __plug_init() {
-        [[ $1 == __asynchronous_plugins ]] && source_cmd=("zsh-defer")
-        source_cmd+="source"
-        local input=${1}
-        set --
-        for plugin in ${${(P)input}}; do
-            unset nosource github_name filename plugindir preload postload postinstall where fetchcommand
-            # split strings by args
-            parts=("${(@s[, ])plugin}")
-            github_name="${parts[1]}"
+        plugindir="${where:-${PLUGROOT}/$github_name}"
 
-            for part in "${parts[@]:1}"; do
-                key="${part%%:*}"
-                value="${part#*:}"
-                case "${key}" in
-                    (if)
-                    eval "${value}" > /dev/null 2>&1 || continue 2
-                    ;;
-                    (nosource)
-                    if [[ "${${value}:l}" == "true" ]] || [[ "${value}" -eq 1 ]]; then
-                        nosource=true
-                    fi
-                    ;;
-                    (postinstall)
-                    postinstall="${postinstall:+$postinstall; }${value}"
-                    ;;
-                    (preload)
-                    preload="${preload:+$preload; }${value}"
-                    ;;
-                    (postload)
-                    postload="${postload:+$postload; }${value}"
-                    ;;
-                    (where)
-                    where="${(e)value}"
-                    ;;
-                    (*)
-                    printf "\r\x1B[31mDid not understand the key: \033[0m\x1B[3m"${part}"\033[0m\nSkipping \x1B[35m"${github_name}"\033[0m plugin\n"
-                    continue 2
-                    ;;
-                esac
-            done
+        if [[ ! -e "${plugindir}" ]]; then
+            printf "\rInstalling \x1B[35m\033[3m${(r:39:)github_name}\033[0m … "
 
-            plugindir="${where:-${PLUGROOT}/$github_name}"
-
-            if [[ ! -e "${plugindir}" ]]; then
-                printf "\rInstalling \x1B[35m\033[3m${(r:39:)github_name}\033[0m … "
-
-                prefix="${github_name:0:4}"
-                if [[ "$prefix" == 'http' ]]; then
-                    filename=("${github_name##*/}")
-                    fetchcommand='curl -L -O "$github_name"'
-                elif [[ "$prefix" == 'git@' ]]; then
-                    fetchcommand='git clone --depth=1 "$github_name" ${plugindir}'
-                else
-                    # we assume github
-                    fetchcommand='git clone --depth=1 "https://github.com/${github_name}.git" ${plugindir}'
-                fi
-
-                if eval "${fetchcommand}" 2> /dev/null; then
-                    printf "\x1B[32m\033[3mSucces\033[0m!\n"
-                if [[ -n $where ]]; then
-                    if [[ $prefix == "http" ]]; then
-                        ln -s "${plugindir}" "${PLUGROOT}/${plugindir##*/}"
-                    else
-                        ln -s "${plugindir}" "${PLUGROOT}/$github_name"
-                        fi
-                    fi
-                else
-                    printf "\r\x1B[31mFAILED\033[0m to install \x1B[35m\033[3m$github_name\033[0m, skipping…\n"
-                    printf "Backtrace:\n"
-                    printf "plugindir: \x1B[32m${plugindir}\033[0m\n"
-                    printf "github_name: \x1B[32m${github_name}\033[0m\n"
-                    continue
-                fi
-
-                if [[ -n ${postinstall} ]]; then
-                maxlength=${${github_name##*/}:0:21}
-                printf "\rPerforming \x1B[34m\033[3m${maxlength}\033[0m post-install hook "
-                printf %$((21 - ${#maxlength}))s…
-                eval "${(e)postinstall}" 1> /dev/null &&\
-                printf " \x1B[32m\033[3mSucces\033[0m!\n" ||\
-                printf "\r\x1B[31mFailed to run postinstall hook for \x1B[35m\033[3m$github_name\033[0m\n"
-                fi
+            prefix="${github_name:0:4}"
+            if [[ "$prefix" == 'http' ]]; then
+                filename=("${github_name##*/}")
+                fetchcommand='curl -L -O "$github_name"'
+            elif [[ "$prefix" == 'git@' ]]; then
+                fetchcommand='git clone --depth=1 "$github_name" ${plugindir}'
+            else
+                # we assume github
+                fetchcommand='git clone --depth=1 "https://github.com/${github_name}.git" ${plugindir}'
             fi
 
-            if [[ -n "${preload}" ]]; then
-                eval "${(e)preload}"
+            if eval "${fetchcommand}" 2> /dev/null; then
+                printf "\x1B[32m\033[3mSucces\033[0m!\n"
+            if [[ -n $where ]]; then
+                if [[ $prefix == "http" ]]; then
+                    ln -s "${plugindir}" "${PLUGROOT}/${plugindir##*/}"
+                else
+                    ln -s "${plugindir}" "${PLUGROOT}/$github_name"
+                    fi
+                fi
+            else
+                printf "\r\x1B[31mFAILED\033[0m to install \x1B[35m\033[3m$github_name\033[0m, skipping…\n"
+                printf "Backtrace:\n"
+                printf "plugindir: \x1B[32m${plugindir}\033[0m\n"
+                printf "github_name: \x1B[32m${github_name}\033[0m\n"
+                continue
             fi
 
-            if [[ -z ${nosource} ]]; then
-                filename="${plugindir}/${${github_name##*/}//.zsh/}.zsh"
+            if [[ -n ${postinstall} ]]; then
+            maxlength=${${github_name##*/}:0:21}
+            printf "\rPerforming \x1B[34m\033[3m${maxlength}\033[0m post-install hook "
+            printf %$((21 - ${#maxlength}))s…
+            eval "${(e)postinstall}" 1> /dev/null &&\
+            printf " \x1B[32m\033[3mSucces\033[0m!\n" ||\
+            printf "\r\x1B[31mFailed to run postinstall hook for \x1B[35m\033[3m$github_name\033[0m\n"
+            fi
+        fi
+
+        if [[ -n "${preload}" ]]; then
+            eval "${(e)preload}"
+        fi
+
+        if [[ -z ${nosource} ]]; then
+            filename="${plugindir}/${${github_name##*/}//.zsh/}.zsh"
+            if [[ ! -f "${filename}" ]]; then
+                filename="${plugindir}/${github_name##*/}.plugin.zsh"
                 if [[ ! -f "${filename}" ]]; then
-                    filename="${plugindir}/${github_name##*/}.plugin.zsh"
-                    if [[ ! -f "${filename}" ]]; then
-                        filename="${plugindir}/${${github_name##*/}//zsh-/}.plugin.zsh"
-                        if [ ! -f "${filename}" ]; then
-                            printf "No filename with the name \"${filename}\"\n"
-                            continue
-                        fi
+                    filename="${plugindir}/${${github_name##*/}//zsh-/}.plugin.zsh"
+                    if [ ! -f "${filename}" ]; then
+                        printf "No filename with the name \"${filename}\"\n"
+                        continue
                     fi
                 fi
-                compile_or_recompile "${filename}"
-                ${source_cmd} "$filename"
             fi
+            compile_or_recompile "${filename}"
+            ${source_cmd} "$filename"
+        fi
 
-            if [[ -n "${postload}" ]]; then
-                eval "${(e)postload}"
-            fi
+        if [[ -n "${postload}" ]]; then
+            eval "${(e)postload}"
+        fi
     done
     unset nosource github_name filename plugindir preload postload postinstall where fetchcommand
+    printf "\x1b[?25h"            # show the cursor again
 }
 
 compile_or_recompile "${ZDOTDIR:-$HOME}/.zshrc"
